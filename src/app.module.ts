8c1647c8480bc21a674ba96d01feabf0634a2ff9
Yehya <yehyam661@gmail.com>
2026-03-25 18:02:09 +0300

    remove readme & some comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-//import { AppController } from './app.controller';
-//import { AppService } from './app.service';
 
-
-// I kept this commented out, because I don't want to use it for now, but I might need it later, so I will keep it here for now.
 
 @Module({
   imports: [],
-  //controllers: [AppController],
- // providers: [AppService],
 })
 export class AppModule {}
