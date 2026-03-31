7ca0705ed85702645ac90546c511b64dc41d8878
Yehya <yehyam661@gmail.com>
2026-03-31 13:48:34 +0300

    add a controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-
+import { HelloController } from './hello.controller';
 
 @Module({
   imports: [],
+  controllers: [HelloController],
 })
 export class AppModule {}
